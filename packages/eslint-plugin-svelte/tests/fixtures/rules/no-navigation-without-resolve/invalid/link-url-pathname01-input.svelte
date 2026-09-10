9ec3c5c8b074026194b20cc76f9ef744e06b065e
Marek Dědič <developer@dedic.eu>
2026-09-10 18:55:47 +0200

    fixup! test(no-navigation-without-resolve): added tests with the URL class

--- packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/invalid/link-url-pathname01-input.svelte
+++ packages/eslint-plugin-svelte/tests/fixtures/rules/no-navigation-without-resolve/invalid/link-url-pathname01-input.svelte
@@ -2,6 +2,5 @@
 	const one = new URL('https://svelte.dev/blog');
 </script>
 
-<!-- .pathname is a bare internal path (/blog); it still needs resolve(). -->
 <a href={one.pathname}>One</a>
 <a href={one.pathname + one.search}>Two</a>
